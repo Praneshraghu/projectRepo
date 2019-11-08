5c1eec755a14c377c1ff06f0adb5e50302a71eaa
Praneshraghu <praneshrpraneshr.18@gmail.com>
2019-11-08 14:45:55 +0530

    Second commit....

---
+++ src/com/gitprojectexample/main_git_sample.java
@@ -0,0 +1,10 @@
+package com.gitprojectexample;
+
+public class main_git_sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Come on done with the job");
+	}
+
+}
